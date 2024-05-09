9fa03a3487a0fd0afe70b21ce90446ef602b61f6
rajdev12 <rjnath98@gmail.com>
2024-05-09 11:32:17 +0530

    Basic structure

--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -21,6 +21,18 @@ function Reviews() {
       name: "Thomas Morgan",
       country: "USA",
     },
+    {
+      src: "https://images.unsplash.com/photo-1554196038-950a8ab51827?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: '"Using Kobodrop has streamlined my financial transactions like never before. Highly recommended for anyone looking to simplify their finances!"',
+      name: "Elena Rodriguez",
+      country: "Spain",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1554196038-950a8ab51827?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: '"Using Kobodrop has streamlined my financial transactions like never before. Highly recommended for anyone looking to simplify their finances!"',
+      name: "Elena Rodriguez",
+      country: "Spain",
+    },
   ];
   return (
     <section
@@ -28,12 +40,14 @@ function Reviews() {
       className="flex max-w-7xl flex-col items-center justify-between px-8 py-20 lg:m-auto lg:flex-row lg:px-12"
     >
       <article className="mb-10 flex flex-col items-center justify-center lg:w-1/2 lg:items-start">
-        <h2 className="mb-4 max-w-[16ch] text-center text-4xl font-semibold text-gray-800 lg:w-full lg:text-left">
+        <h2 className="mb-4 max-w-[17ch] text-center text-4xl font-semibold text-gray-800 lg:w-full lg:text-left">
           Join other thousands of people growing with Kobodrop
         </h2>
         <StoreLinks type={BtnTypes.Standard} />
       </article>
+      <Carousel slides={slides}/>
       <Carousel slides={slides} />
+      
     </section>
   );
 }
